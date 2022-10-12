be32255ec026c7f6ac97ec5ed27cffab59b64e98
Azad Mahmud <azadmahmud2k@gmail.com>
2022-10-13 00:53:49 +0600

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -8,14 +8,14 @@ public class Operations {
     
 
     int sum1 = 0;
-    public int getSum1() {
+    public static int getSum1() {
         for(int i = 1; i < array.length; i++){
             sum1 = sum1 + array[i];
         }
         return sum1;
     }
     int sum2 = 0;
-    public int getSum2(){
+    public static int getSum2(){
         for (int i = 1; i < array.length; i++){
             sum2 = sum2 + array[i];
         }
@@ -24,11 +24,13 @@ public class Operations {
 
     int diff = 0;
 
-    public int getdiff(){
+    public static int getdiff(){
         diff = sum1 - sum2;
         return diff;
     }
     
-    
+    public static void main(String[] args){
+        
+    }
 
 }   
